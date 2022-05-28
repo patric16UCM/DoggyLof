c0192f0abf3c2364f83ef801f6a2d5dcb1db9f8d
patric16UCM <93123535+patric16UCM@users.noreply.github.com>
2022-05-28 17:17:15 +0200

    Add files via upload

---
+++ Scripts/popupInicio.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    $('#prueba').css('display','flex')
+   
+  
+     
+    $('#popup-close4').click(function(){
+        $('#prueba').css('display','none')
+     
+    });
+    $('#cerrar').click(function(){
+        $('#prueba').css('display','none')
+     
+    });
+});
+ 
+
+    // JavaScript source code
+    
