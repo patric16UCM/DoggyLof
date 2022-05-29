37d13cf04043e843de632dcfd389adf9215530d9
patric16UCM <patric16@ucm.es>
2022-05-29 11:57:15 +0200

    visual change

--- Scripts/popup3.js
+++ Scripts/popup3.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         document.getElementById('numCant1').innerHTML =2*num;
         document.getElementById('numCant2').innerHTML =2*num;
         document.getElementById('numCant3').innerHTML =num;
-        document.getElementById('precioPack').innerHTML =num*15;
+        document.getElementById('precioPack').innerHTML =(9/7*num*3 + 3*8/7*num + 1*16/7*num).toFixed(2);
     });
     document.getElementById('boton4').addEventListener('click', () => {
         $('.alimentos').css('display','flex')
@@ -16,17 +16,19 @@ $( document ).ready(function() {
     $('#popup-close3').click(function(){
         $('.popup-wrapper3').css('display','none')
         $('.alimentos').css('display','flex')
+        $('#ayudaBtn').css('display','flex')
     });
 });
  
 function abrirPopUP3(){
     var num =  $('#numVal').val();
-    document.getElementById('numCant1').innerHTML =2*num;
-    document.getElementById('numCant2').innerHTML =2*num;
+    document.getElementById('numCant1').innerHTML =3*num;
+    document.getElementById('numCant2').innerHTML =3*num;
     document.getElementById('numCant3').innerHTML =num;
-    document.getElementById('precioPack').innerHTML =num*15;
+    document.getElementById('precioPack').innerHTML =(9/7*num*3 + 3*8/7*num + 16/7*num).toFixed(2);
     $('.popup-wrapper3').css('display','block')
     $('.alimentos').css('display','none')
+    $('#ayudaBtn').css('display','none')
 }
     // JavaScript source code
     
